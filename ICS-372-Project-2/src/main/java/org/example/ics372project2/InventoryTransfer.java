7a3f4c56ffc6d6bc32857b373b6117dc338552a2
maida209 <maida.a.ismail@gmail.com>
2025-03-26 00:28:22 -0500

    Changed the transfer Inventory.

--- ICS-372-Project-2/src/main/java/org/example/ics372project2/InventoryTransfer.java
+++ ICS-372-Project-2/src/main/java/org/example/ics372project2/InventoryTransfer.java
@@ -1,5 +1,6 @@
 package org.example.ics372project2;
 
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -7,6 +8,54 @@ import java.util.Set;
  * transfers inventory from one dealer to another
  */
 public class InventoryTransfer {
+    /**
+     * Transfers a vehicle from one dealer to another.
+     *
+     * @param dealerSet The set of all dealers.
+     * @param dealerIdFrom The ID of the dealer transferring the vehicle.
+     * @param dealerIdTo The ID of the dealer receiving the vehicle.
+     * @param vehicleId The ID of the vehicle being transferred.
+     * @throws IOException If there is an error updating the inventory file.
+     * @return true if the transfer was successful, false otherwise.
+     */
+    public boolean transferVehicle(Set<Dealer> dealerSet, String dealerIdFrom, String dealerIdTo, String vehicleId) throws IOException {
+
+        // validate dealer IDs
+        Dealer dealerFrom = findDealer(dealerIdFrom, dealerSet);
+        Dealer dealerTo = findDealer(dealerIdTo, dealerSet);
+
+        if (dealerFrom == null || dealerTo == null) {
+            System.out.println("One or both Dealer IDs are invalid.");
+            return false;
+        }
+
+        if (dealerFrom.getVehicleList().isEmpty()) {
+            System.out.println("Dealer " + dealerIdFrom + " has no vehicles to transfer.");
+            return false;
+        }
+
+        if (dealerIdFrom.equals(dealerIdTo)) {
+            System.out.println("Cannot transfer vehicle to the same dealer.");
+            return false;
+        }
+
+        Vehicle vehicle = findVehicleById(dealerFrom, vehicleId);
+        if (vehicle == null) {
+            System.out.println("Vehicle ID not found.");
+            return false;
+        }
+
+        // Perform transfer
+        dealerFrom.removeVehicle(vehicle);
+        dealerTo.addVehicle(vehicle);
+
+        System.out.println("Vehicle " + vehicleId + " successfully transferred from " +
+                dealerIdFrom + " to " + dealerIdTo + ".");
+
+        File_Writer.exportJSON(dealerSet);
+        return true;
+    }
+
 
     /**
      * helper method that returns the dealerId from the set given to
